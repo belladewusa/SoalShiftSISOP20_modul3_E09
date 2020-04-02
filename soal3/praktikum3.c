23c42d6d3d98dae1aec88061332c8a18f880d069
EdoYogatama <roronoadezoro@gmail.com>
2020-04-02 15:34:30 -0400

    udah bisa -f yang ada file extension

--- soal3/praktikum3.c
+++ soal3/praktikum3.c
@@ -10,20 +10,23 @@
 #include <pthread.h>
 #include <errno.h>
 #include <dirent.h>
+#include <ctype.h>
 
-// void* categorize_it(void* argv){
-//     char* folder = (char*)argv;
-//     if(mkdir(folder,S_IRUSR | S_IWUSR | S_IXUSR)){
-//         printf("success\n");
-//     } else {
-//         exit(EXIT_FAILURE);
-//     }
-// }
+int is_dir(const char* path) {
+    struct stat buf;
+    stat(path, &buf);
+    return S_ISDIR(buf.st_mode);
+}
 
 int is_ext(char fileext[],int len_ext,char* dird_name,int len_file) {
     int count = 0;
+    // printf("masuk\n");
     for (int i = 1; i <= len_ext; i++){
-        if(fileext[len_ext-i]==dird_name[len_file-i]){
+        if(len_ext-i==-1||len_file-i==-1){
+            break;
+        }
+        if(fileext[len_ext-i]==dird_name[len_file-i]||toupper(fileext[len_ext-i])==dird_name[len_file-i]){
+            // printf("%c->%c\n",fileext[len_ext-i],dird_name[len_file-i]);    
             count++;
         }
     }
@@ -35,11 +38,60 @@ int is_ext(char fileext[],int len_ext,char* dird_name,int len_file) {
     }
 }
 
-int main(int argc,char* argv[]){
-    int f_or_d = 0;
+void* dash_f(void* argv){
+    char *ext = (char*)argv;
     char folder[100];
     int len_ext,len_file;
     char fileext[10];
+    if (!ext) {
+        sprintf(folder,"/home/edo/Kuliah/Sisop/SoalShiftSISOP20_modul3_E09/soal3/soal3/Unknown");
+    } else {
+        // printf("extension is %s\n", ext);
+        sprintf(folder,"/home/edo/Kuliah/Sisop/SoalShiftSISOP20_modul3_E09/soal3/soal3/%s",ext + 1);
+        // pthread_create(&threads[count++],NULL,&categorize_it,(void*)ext);
+        sprintf(fileext,"%s",ext);
+        len_ext = strlen(fileext);
+        if(mkdir(folder,S_IRUSR | S_IWUSR | S_IXUSR)==0){
+            printf("success\n");
+            DIR *d;
+            struct dirent *dir;
+            d = opendir("./soal3");
+            if (d)
+            {
+                while ((dir = readdir(d)) != NULL){
+                    // printf("%s\n",dir->d_name);
+                    char tmp[150],new[150];
+                    memset(tmp,0,150*sizeof(char));
+                    memset(new,0,150*sizeof(char));
+                    strcpy(tmp,"/home/edo/Kuliah/Sisop/SoalShiftSISOP20_modul3_E09/soal3/soal3/");
+                    sprintf(new,"%s/",folder);
+                    len_file = strlen(dir->d_name);
+                    if (is_ext(fileext,len_ext,dir->d_name,len_file)){
+                        // printf("di if\n");
+                        strcat(tmp,dir->d_name);
+                        strcat(new,dir->d_name);
+                        if(is_dir(tmp)){
+                            continue;
+                        } else {
+                            rename(tmp,new);
+                        }
+                    }                                
+                }
+                closedir(d);
+            }
+            // exit(EXIT_SUCCESS);
+        } else {
+            printf("Error %s\n",strerror(errno));
+        }
+    }
+}
+
+int main(int argc,char* argv[]){
+    int f_or_d = 0;
+    // char folder[100];
+    // int len_ext,len_file;
+    // char fileext[10];
+    int err;
     pthread_t *threads; 
 	threads = (pthread_t*)malloc(argc*sizeof(pthread_t));
     if(strcmp(argv[1],"-f")==0){
@@ -56,44 +108,16 @@ int main(int argc,char* argv[]){
     if(f_or_d==1){
         int count = -1;
         for (int i = 2; i < argc; i++){
+            // count++;
             // printf("%s ",argv[i]);
             char *ext = strrchr(argv[i], '.');
-            if (!ext) {
-                sprintf(folder,"/home/edo/Kuliah/Sisop/SoalShiftSISOP20_modul3_E09/soal3/soal3/Unknown");
-            } else {
-                // printf("extension is %s\n", ext);
-                sprintf(folder,"/home/edo/Kuliah/Sisop/SoalShiftSISOP20_modul3_E09/soal3/soal3/%s",ext + 1);
-                // pthread_create(&threads[count++],NULL,&categorize_it,(void*)ext);
-                sprintf(fileext,"%s",ext);
-                len_ext = strlen(fileext);
-                if(mkdir(folder,S_IRUSR | S_IWUSR | S_IXUSR)==0){
-                    printf("success\n");
-                } else {
-                    printf("Error %s\n",strerror(errno));
-                    DIR *d;
-                    struct dirent *dir;
-                    d = opendir("./soal3");
-                    if (d)
-                    {
-                        while ((dir = readdir(d)) != NULL){
-                            char tmp[100];
-                            strcpy(tmp,"/home/edo/Kuliah/Sisop/SoalShiftSISOP20_modul3_E09/soal3/soal3/");
-                            strcpy(tmp,dir->d_name);
-                            len_file = strlen(dir->d_name);
-                            if (is_ext(fileext,len_ext,dir->d_name,len_file)){
-                                printf("%s\n",dir->d_name);
-                            }                                
-                        }
-                        closedir(d);
-                    }
-                    exit(EXIT_FAILURE);
-                }
-            }
+            // dash_f((void*)ext);
+            pthread_create(&threads[++count],NULL,dash_f,(void*)ext);
         }
         printf("selesai\n");
-        // for(int i=0;i<=count;i++){
-        //     pthread_join(threads[i],NULL);
-        // }
+        for(int i=0;i<=count;i++){
+            pthread_join(threads[i],NULL);
+        }
         
     }
     
